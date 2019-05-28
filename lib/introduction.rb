c307adc711c962c89f5df2b49bb98579f1ecc1dd
Melanie Schicker <melanie.schicker@gmail.com>
2019-05-28 15:51:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,5 +5,9 @@ def introduction(name)
 end
 
 def introduction_with_language("Dan","Ember.js")
+<<<<<<< HEAD
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+=======
+  puts "Hi, my name is #{name} and I am learning to program in #{language}\n"
+>>>>>>> 47c83bb773ab88a053f3072e0e859cd2d8caf8c1
 end
